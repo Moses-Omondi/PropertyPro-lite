47684237d68ede22224e0d2446c7d02f76f56875
Moses-Omondi <mose.omondi@gmail.com>
2019-07-12 05:26:44 +0530

    Update user and property model

---
+++ server/model/propertiesmodel.js
@@ -0,0 +1,38 @@
+/* eslint-disable import/prefer-default-export */
+export const property = [
+  {
+    owner: 'Micheal Carrick',
+    status: 'status',
+    price: '340000',
+    state: 'Kigali',
+    city: 'Kigali',
+    address: 'Homeland',
+    type: 'sell',
+    created_on: '2-12-2019',
+    image_url: 'image.jpg',
+  },
+
+  {
+    owner: 'Julius Duek',
+    status: 'status',
+    price: '340000',
+    state: 'France',
+    city: 'Paris',
+    address: 'Jupiter',
+    type: 'sell',
+    created_on: '2-8-2019',
+    image_url: 'image.jpg',
+  },
+
+  {
+    owner: 'David Crack',
+    status: 'status',
+    price: '340000',
+    state: 'Germany',
+    city: 'Westend',
+    address: 'Koln',
+    type: 'sell',
+    created_on: '1-12-2019',
+    image_url: 'image.jpg',
+  },
+];
