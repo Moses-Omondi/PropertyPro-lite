a30fcc15e040688b0b94cd416bfbc0c7cff27593
Moses-Omondi <mose.omondi@gmail.com>
2019-07-12 21:35:07 +0530

    Controller update

--- server/controller/userscontroller.js
+++ server/controller/userscontroller.js
@@ -1,14 +1,46 @@
-/* eslint-disable no-unused-expressions */
+/* eslint-disable semi */
+/* eslint-disable no-const-assign */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 /* eslint-disable quotes */
-import users from '../model/usersmodel';
+/* eslint-disable linebreak-style */
+/* eslint-disable no-empty-function */
+/* eslint-disable sort-keys */
+/* eslint-disable no-magic-numbers */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable linebreak-style */
+/* eslint-disable eqeqeq */
+/* eslint-disable no-shadow */
+/* eslint-disable one-var */
+/* eslint-disable linebreak-style */
+import users from "../models/users";
+export const getAllUsers = (req, res) => res.send(users);
 
-// create user controller
+export const getUserById = (req, res) => {
+
+    const user = users.find((users) => users.id == req.params.id);
+
+    return res.send(user);
+
+};
 
 export const createUser = (req, res) => {
-  const { body } = req;
-  users.push[body];
-  return res.status(201).json({
-    status: "success",
-    data: body,
-  });
+
+    const newUser = {
+        email,
+        first_name,
+        last_name,
+        password,
+        phoneNumber,
+        address,
+        is_admin
+    };
+
+    newUser = body.req
+
+    return res.status(201).json({
+        "status": "success",
+        "data": body
+    });
+
 };
