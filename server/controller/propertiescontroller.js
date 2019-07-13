0391c3178b45ca47f8000316ec34983b8b1c688d
Moses-Omondi <mose.omondi@gmail.com>
2019-07-13 06:14:08 +0530

    FT[API update]

--- server/controller/propertiescontroller.js
+++ server/controller/propertiescontroller.js
@@ -1,10 +1,12 @@
+/* eslint-disable max-len */
 /* eslint-disable consistent-return */
 /* eslint-disable eqeqeq */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-console */
 /* eslint-disable no-shadow */
 /* eslint-disable one-var */
-import properties from '../models/properties';
+import properties from '../model/propertiesmodel';
+import responses from '../helpers/responses';
 
 export const getAllProperties = (req, res) => {
     if (properties.length == 0) {
@@ -62,4 +64,22 @@ export const createProperty = (req, res) => {
     });
 
 
+};
+
+export const deleteProperty = (req, res) => {
+    const { id } = req.body,
+
+    const index = properties.findIndex((properties) => properties.id == req.params.id);
+
+    if(index !== -1){
+        properties.splice(index, 1);
+        return res.status(200).json({
+            status: 'ok',
+            data: 'property deleted'
+        })
+    }
+    res.status(404).json({
+        status: 'error',
+        data: 'no property not found'
+    })
 };
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,19 +1,21 @@
+/* eslint-disable no-undef */
 /* eslint-disable max-len */
 import express from 'express';
-import { createUser, getAllUsers, getUserById } from '../controllers/userControl';
-import { createProperty, getAllProperties, getPropertyById, getPropertyByType } from '../controllers/propertyControl';
+import { createUser, getAllUsers, getUserById } from '../controller/userscontroler';
+import { createProperty, getAllProperties, getPropertyById, getPropertyByType } from '../controller/propertiescontroller';
 const router = express.Router();
 
 router.get('/users', getAllUsers);
-router.get('/users/:id', getUserById);
+router.get('/users/:Id', getUserById);
 router.post('/user', createUser);
 
 // router.post("/user/login", signinUser)
 
 router.get('/properties', getAllProperties);
-router.get('/properties/:id', getPropertyById);
+router.get('/properties/:Id', getPropertyById);
 router.get('/properties/type/:type', getPropertyByType);
 router.post('/property', createProperty);
+router.delete('/properlete', deleteProperty);
 
 
 export default router;
